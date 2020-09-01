4f6bb7fc9f5dc0f6e0eb6a73c59c91ea1d23b546
Franciscoas4 <franciscoas4@gmail.com>
2020-08-31 22:07:25 -0300

    Inicio da criação da interface gráfica do jogo(criação do tabuleiro).

---
+++ src/br/com/aula/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package br.com.aula.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.aula.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel{
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+}
