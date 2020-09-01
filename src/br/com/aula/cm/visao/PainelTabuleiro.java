6a9e193524e17a9c8730dcc0cb6e9026cf8ccb1e
Franciscoas4 <franciscoas4@gmail.com>
2020-08-31 23:46:30 -0300

    Finalização. Correção de bugs. Inserção da chamada de vitória ou derrota na classe PainelTabuleiro.

--- src/br/com/aula/cm/visao/PainelTabuleiro.java
+++ src/br/com/aula/cm/visao/PainelTabuleiro.java
@@ -2,7 +2,9 @@ package br.com.aula.cm.visao;
 
 import java.awt.GridLayout;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import br.com.aula.cm.modelo.Tabuleiro;
 
@@ -16,7 +18,16 @@ public class PainelTabuleiro extends JPanel{
 		tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
 		
 		tabuleiro.registrarObservador(e -> {
-			// TODO mostrar resultado para o usuário
+			
+			SwingUtilities.invokeLater(() -> {
+				if(e.isGanhou()) {
+					JOptionPane.showMessageDialog(this, "Ganhou :)");
+				} else {
+					JOptionPane.showMessageDialog(this, "Perdeu :(");
+				}
+				
+				tabuleiro.reiniciar();
+			});
 		});
 	}
 }
