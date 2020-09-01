76a335e4c7f97766e2ae9a33efaae2076d1d4d33
Franciscoas4 <franciscoas4@gmail.com>
2020-08-31 22:22:56 -0300

    Criação dos campos do jogo dentro do tabuleiro utilizando o Swing

---
+++ src/br/com/aula/cm/visao/BotaoCampo.java
@@ -0,0 +1,13 @@
+package br.com.aula.cm.visao;
+
+import javax.swing.JButton;
+
+import br.com.aula.cm.modelo.Campo;
+
+@SuppressWarnings("serial")
+public class BotaoCampo extends JButton{
+
+	public BotaoCampo(Campo campo) {
+		
+	}
+}
